706d699b99e7a0c558f6293669db5b3bcbf612c1
Leo <kl@alushta.net>
2018-10-12 17:06:17 +0300

    Keep values on post while choose it

--- app.rb
+++ app.rb
@@ -25,17 +25,22 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 
-	c = Client.new params[:client]
-	if c.save
+	@c = Client.new params[:client]
+	if @c.save
 		erb 'Вы записались)'
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end
 end
 
+get '/barber/:id' do
+	@barber = Barber.find(params[:id])
+	erb :barber
+end
